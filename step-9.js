297406d0be7f9667c990c42143ff0a80b2fcb75c
dfdev2 <132777056+dfdev2@users.noreply.github.com>
2024-12-11 20:34:04 +0100

    step 9

---
+++ step-9.js
@@ -0,0 +1,12 @@
+// Step 9
+// When you declare a variable without initializing it, it is considered uninitialized.
+// Currently, your secondCharacter variable is uninitialized.
+// Add a console.log() to see what the value of your secondCharacter variable is.
+
+// Когда вы объявляете переменную без ее инициализации, она считается неинициализированной.
+// В настоящее время ваша переменная secondCharacter является неинициализированной.
+// Добавьте console.log(), чтобы узнать значение переменной secondCharacter.
+let character = "Hello";
+console.log(character);
+character = "World";
+let secondCharacter;
