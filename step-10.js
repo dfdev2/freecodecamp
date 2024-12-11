4860dd0424b7c1a848bae79e800513385c44a88f
dfdev2 <132777056+dfdev2@users.noreply.github.com>
2024-12-11 20:37:53 +0100

    step 10

---
+++ step-10.js
@@ -0,0 +1,24 @@
+// Step 10
+// The default value of an uninitialized variable is undefined.
+// This is a special data type that represents a value that does not have a definition yet.
+// Значение по умолчанию для неинициализированной переменной - undefined.
+// Это специальный тип данных, представляющий значение, у которого еще нет определения.
+
+// You can still assign a value to an uninitialized variable. Here is an example:
+// Вы все еще можете присвоить значение неинициализированной переменной. Вот пример:
+
+// Example Code
+// let uninitialized;
+// uninitialized = "assigned";
+// Assign the string "Test" to your secondCharacter variable below your declaration.
+// Open the console to see how your log has changed.
+
+// Присвойте переменной secondCharacter строку "Test", расположенную под объявлением.
+// Откройте консоль, чтобы посмотреть, как изменился ваш журнал.
+
+let character = "Hello";
+console.log(character);
+character = "World";
+let secondCharacter;
+secondCharacter = "Test";
+console.log(secondCharacter);
