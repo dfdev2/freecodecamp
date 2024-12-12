b9aaefe5ecb1abf912ab882527f7f4ed9451342d
dfdev2 <132777056+dfdev2@users.noreply.github.com>
2024-12-12 17:22:24 +0100

    step 11

---
+++ step-11.js
@@ -0,0 +1,15 @@
+// You can also assign the value of a variable to another variable. For example:
+// Вы также можете присвоить значение переменной другой переменной. Например:
+
+// The second variable would now have the value "One".
+
+// To see this in action, change your secondCharacter assignment from "Test" to your character variable.
+
+// Then open the console to see what gets logged.
+
+let character = "Hello";
+console.log(character);
+character = "World";
+let secondCharacter;
+secondCharacter = "Test";
+console.log(secondCharacter);
