08b6b3bf2bbbc68345ed16faa2273911f6d783db
dfdev2 <132777056+dfdev2@users.noreply.github.com>
2024-12-12 17:26:22 +0100

    step-12

---
+++ step-12.js
@@ -0,0 +1,6 @@
+// Step 12
+// You are now ready to declare your next variable. Remove both console.log statements, and the character reassignment.
+
+// Also remove your secondCharacter variable, but leave the character initialization unchanged.
+
+let character = "Hello";
