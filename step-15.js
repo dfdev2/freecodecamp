80352273d9a0e224839cc7c4e3c0bea786621d9d
dfdev2 <132777056+dfdev2@users.noreply.github.com>
2024-12-12 17:32:45 +0100

    step 15

---
+++ step-15.js
@@ -0,0 +1,6 @@
+// Step 15
+// Use let to declare a count variable. Assign it the number 8.
+// When using a number value, you do not use quotes.For example:
+
+let character = "Hello";
+let count = 8;
